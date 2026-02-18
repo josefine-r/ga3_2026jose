93b2f4e83fb59690c78a3d1902e6fab09a7cd2f6
Josefine-R <josefinebirgitte@icloud.com>
2026-02-18 19:58:27 +0100

    new Door script

---
+++ Assets/Script/MyDoorController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class MyDoorController : MonoBehaviour
+{
+    private Animator doorAnim;
+
+    private bool doorOpen = false;
+
+    private void Awake()
+    {
+        doorAnim = gameObject.GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        if (!doorOpen)
+        {
+            doorAnim.Play("DoorOpen", 0, 0.0f);
+            doorOpen = true;
+        }
+        else
+        {
+            doorAnim.Play("DoorClose", 0, 0.0f);
+            doorOpen = false;
+        }
+    }
+}
